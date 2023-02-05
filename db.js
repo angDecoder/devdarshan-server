8e6f3d71a82921a6a84f225cfb030ad5204ed44d
angDecoder <angshudas012@gmail.com>
2023-02-05 20:23:57 +0530

    updated db

--- db.js
+++ db.js
@@ -11,8 +11,7 @@ const pool = new Pool({
     database : PGDATABASE,
     ssl : true
 })
-'PGPASSWORD=QCBpAU3VAp9zAuzWL4uB7pZ3rTZJfYmW psql -h dpg-cffko402i3mg6p91udfg-a.oregon-postgres.render.com -U devdarshan_user devdarshan'
-'postgres://devdarshan_user:QCBpAU3VAp9zAuzWL4uB7pZ3rTZJfYmW@dpg-cffko402i3mg6p91udfg-a/devdarshan'
+
 module.exports = pool;
 
 // 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
